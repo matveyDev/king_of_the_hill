dfe8a2956c593170dcbd993cc676c1e2f609b1a9
Matvey Semenov <matveytri777@gmail.com>
2022-12-10 16:40:23 +0200

    clear inline style

--- frontend/src/components/static/Navbar.jsx
+++ frontend/src/components/static/Navbar.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ButtonConnect from '../UI/buttons/ButtonConnect';
 import ButtonDisconnect from '../UI/buttons/ButtonDisconnect';
 
+
 const Navbar = () => {
   return (
-    <div style={{marginBottom: '15px'}}>
-      <nav class="blue lighten-2">
-        <div class="nav-wrapper container">
-          <a href="/" class="brand-logo">KingOfTheHill</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <ButtonConnect/>
-            <ButtonDisconnect/>
+    <div className='navbar'>
+      <nav className="blue lighten-2">
+        <div className="nav-wrapper container">
+          <a href="/" className="brand-logo">KingOfTheHill</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <ButtonConnect />
+            <ButtonDisconnect />
           </ul>
         </div>
       </nav>
