a1a56fb0593b867a910b1994117609dc3dab3b92
Matvey Semenov <matveytri777@gmail.com>
2022-12-11 01:26:55 +0200

    clean left-over

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Web3 from 'web3';
 
-import config from './config.json';
-import HomePage from "./pages/Home";
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import './styles/App.css';
+import config from './config.json';
+import HomePage from "./pages/Home";
 import Navbar from './components/static/Navbar';
 
 
@@ -14,7 +13,6 @@ export const web3 = new Web3(Web3.givenProvider);
 export const contractInstance = new web3.eth.Contract(
   config.ABI, config.CONTRACT_ADDRESS
 );
-export const wsClient = new W3CWebSocket(config.WEBSOCKET_URL);
 
 
 const App = () => {
