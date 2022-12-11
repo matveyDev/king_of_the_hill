afa82b44a5d79f54f189a742686417d2daf3b732
Matvey Semenov <matveytri777@gmail.com>
2022-12-11 15:14:46 +0200

    add endpoint get user by field

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,17 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from database.schemas import User as UserSchema
+from database.mongodb import MongodbAPI
 
+app = FastAPI()
+mongodb = MongodbAPI()
+
+
+@app.get('/users_by', response_model=list[UserSchema])
+async def get_users_by(field: str, limit: int):
+    users = mongodb.get_users_by(
+        field=field,
+        order_by=-1,
+        limit=limit
+    )
+    return users
