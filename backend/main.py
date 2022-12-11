b8dfda8971739633cf0feef7d38de3fc31567780
Matvey Semenov <matveytri777@gmail.com>
2022-12-11 15:46:46 +0200

    add two endpoints

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,25 @@ app = FastAPI()
 mongodb = MongodbAPI()
 
 
+@app.post('/add_user', response_model=UserSchema)
+async def add_user(address: str):
+    if mongodb.check_user_exists(address) is True:
+        raise Exception('User allready exists')
+
+    mongodb.add_user(address)
+    user = mongodb.top_users.find_one({'address': address})
+
+    return UserSchema(**user)
+
+
+@app.patch('/add_prize', response_model=UserSchema)
+async def add_prize(address: str, prize: int):
+    mongodb.add_prize_to_user(address, prize)
+    user = mongodb.top_users.find_one({'address': address})
+
+    return UserSchema(**user)
+
+
 @app.get('/users_by', response_model=list[UserSchema])
 async def get_users_by(field: str, order_by: int = -1, limit: int = 5):
     users = mongodb.get_users_by(
