2949e19a9788f3f48e2ae451a1f53bae05b551d8
Matvey Semenov <matveytri777@gmail.com>
2022-12-12 02:41:31 +0200

    fixed css

--- frontend/src/components/panels/TopUsersPanel.jsx
+++ frontend/src/components/panels/TopUsersPanel.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import '../../styles/topUsersPanel.css';
 import config from '../../config.json';
+import { contractInstance, web3 } from '../../App';
 
 
 const TopUsersPanel = () => {
@@ -9,7 +10,7 @@ const TopUsersPanel = () => {
 
   const getTopUsers = async () => {
     return fetch(`${config.SERVER_URL}users_by?field=total_prizes&limit=5`,
-      { 'method': 'GET' }
+      { method: 'GET' }
     ).then(data => data.json())
   };
 
@@ -17,23 +18,58 @@ const TopUsersPanel = () => {
     getTopUsers().then(_users => {
       setUsers(_users);
     })
+
   }, []);
 
-  return (
-    <div className='panel panel__top_users z-depth-5'>
-      <div className='panel_text title_top_users'>Top 5 Winenrs:</div>
-      {users.map((user) => {
+  const addNewUser = async (address) => {
+    return fetch(`${config.SERVER_URL}add_user?address=${address}`,
+      { method: 'POST' }
+    ).catch(error => console.log(error));
+  };
+
+  const addPrize = async (address, prize) => {
+    const _prize = web3.utils.fromWei(prize, 'ether');
+    return fetch(`${config.SERVER_URL}add_prize?address=${address}&prize=${_prize}`,
+      { method: 'PATCH' }
+    ).catch(error => console.log(error));
+  };
+
+  contractInstance.events.NewWinner(
+    async (error, event) => {
+      if (error) { console.log(error); };
+      addNewUser(event.returnValues._address);
+      addPrize(
+        event.returnValues._address,
+        event.returnValues._prize
+      ).
+      then(getTopUsers().then(_users => {
+        setUsers(_users);
+      }));
+    }
+  );
+
+  const renderTopUsers = () => {
+    return (
+      users.map((user) => {
+        const _address = `${user.address.slice(0, 6)}...${user.address.slice(user.address.length - 4)}`;
+        const _total_prizes = String(user.total_prizes).slice(0, 6) + ' ETH';
         return (
           <a
             className='panel_text panel__top_users_address'
             target='_blank'
             key={user.address}
             href={config.ETHERSCAN_URL + user.address}
-          >{user.address} ({user.total_prizes} ETH)
+          >{_address} ({_total_prizes})
           </a>
         );
       })
-      }
+    );
+  };
+
+  return (
+    <div className='panel panel__top_users z-depth-5'>
+      <div className='panel_text title_top_users'>Top 5 Winenrs:</div>
+      {renderTopUsers()}
     </div>
   );
 };
